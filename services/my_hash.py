44216c0de3d8e1ee525df082fd2a569a262af551
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-07-12 11:51:45 -0300

    atualização em services/my_hash

--- services/my_hash.py
+++ services/my_hash.py
@@ -10,3 +10,12 @@ def hash_password(password: str) -> str:
     hashed = bcrypt.hashpw(password_bytes, salt)
 
     return hashed.decode("utf-8")
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+
+    password_bytes = password.encode("utf-8")
+
+    hashed_bytes = hashed_password.encode("utf-8")
+
+    return bcrypt.checkpw(password_bytes, hashed_bytes)
